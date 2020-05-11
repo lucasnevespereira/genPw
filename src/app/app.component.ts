742469c28ad5bfd05053ee51b107d6439b382926
lucas.nevespereira <lucas.nevespereira@eemi.com>
2020-05-11 20:49:38 +0200

    inputs methods added and handled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,37 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  length: number = 0;
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+  password: string;
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
   onButtonClick() {
-    console.log("Got Clicked");
+    console.log(this.includeLetters);
+    console.log(this.includeNumbers);
+    console.log(this.includeSymbols);
+
+    this.password = "My Password";
   }
 }
